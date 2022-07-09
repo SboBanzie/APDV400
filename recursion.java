fa90b5e30b5728e3b6da63078295b2fd2fb435ff
Abo <Abo@122>
2022-07-09 17:28:43 +0200

    the recursion code

---
+++ recursion.java
@@ -0,0 +1,9 @@
+public class add{
+   public static vod main (String[]arg){
+            int n= 0;
+            system.out.println("the number is "+n)
+            numberToString(n);
+
+            static void numberToString(n){
+            if (n==0) return;
+            String s[]={"zero","one","two"}
